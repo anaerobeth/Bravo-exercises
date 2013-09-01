b59050fe953e2baaebd8d3512b54cd12abab6587
Beth Tenorio <betsky98@gmail.com>
2013-08-31 21:21:42 -0400

    Add classes Student and Assignment Grade

--- bravo_projects/teaching_assistant.rb
+++ bravo_projects/teaching_assistant.rb
@@ -1,20 +1,57 @@
 require 'CSV'
 
+# GradeReader - an object that is responsible for reading in grade data from a CSV.
 class GradeReader
-
   def read(file)
-  student_record = {}
+  student_file = []
   grades = []
-  puts student_record
-  puts grades
     CSV.foreach(file) do |row|
-      student_name = row.shift
-      grades = row
-      student_record[student_name] =  grades
+    student_file << row
     end
-    puts student_record
+    return student_file
   end
 end
 
-record = GradeReader.new()
-record.read('student_grade_sample.csv')
+
+# Student - an object that represents a participant in a class
+class Student
+  attr_accessor :student_names
+
+  def get_name(item)
+    name = item[0]
+    return name
+  end
+
+end
+
+
+# AssignmentGrade - an object that encapsulates the concept of a given assignment grade
+class AssignmentGrade
+  attr_accessor :grade
+
+  def get_grades(item)
+    grades = [item[1], item[2], item[3], item[4], item[5]]
+    return grades
+  end
+
+
+end
+
+reader = GradeReader.new()
+student = Student.new()
+grade = AssignmentGrade.new()
+
+
+student_records = {}
+student_file = reader.read('student_grade_sample.csv')
+puts student_file
+student_file.each do |record|
+  name = student.get_name(record)
+  grades = grade.get_grades(record)
+  student_records[name] =  grades
+end
+
+puts student_records
+
+
+
