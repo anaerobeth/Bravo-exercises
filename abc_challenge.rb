3b4d6fe46b848429eb842a1e9a15eb91b9686880
Beth Tenorio <betsky98@gmail.com>
2013-09-02 13:03:24 -0400

    Working method to build array of valid abcs

--- abc_challenge.rb
+++ abc_challenge.rb
@@ -2,15 +2,37 @@ s0 = 'abc'  #1
 s1 = 'aabc' #2
 s2 = 'aabbc' #4
 s3 = 'abcbc' #3
+s4 = 'abcabc' #4
 s5 = 'bcd' #0
 string = 'aacacabaaaaacccbccbcccababbccbcccbbbbbbacabbacaaaabbbabcabaccbaccabaaabccabacbabaaabaabcabacccabccaababcbacaaabaccaccacccbabbbbbaaacaaacacccaaabcbcabbacccccbaaacbbaccbbcaabcbabbaabbacbccccbaaccccbccacbaaabcaaacaabbcabcbabcbaccbaacbaaabcacccbbabccbbbcacabccccabbcaaaaacaabbaaccabbbccababbbcbabaabcbaabcacbaaaaccccbbcccacabbacccacaaaccbabaabbbcabbabac'
 #pattern = /a+b+c/
 #puts s3.scan(pattern)
 
 array = [s0, s1, s2, s3, s5, string]
-total_a = s3.count('a')
-total_b = s3.count('b')
-total_c = s3.count('c')
-total_abc = total_a * total_b * total_c
-puts total_abc
+
+# make an array of letter indexes
+  puts "Sample string: 'abcabc'"
+  a_indexes = [0, 3]
+  b_indexes = [1, 4]
+  c_indexes = [2, 5]
+
+
+  def build_abc(a_indexes, b_indexes, c_indexes)
+    count = 0
+    a_indexes.each do |a|
+      #puts a
+      b_indexes.each do |b|
+        #puts b
+        c_indexes.each do |c|
+          #puts c
+          if a < b && b < c
+            puts "Valid abc found: #{a}, #{b}, #{c}"
+            count += 1
+          end
+        end
+      end
+    end
+    return count
+  end
+  puts build_abc(a_indexes, b_indexes, c_indexes)
 
