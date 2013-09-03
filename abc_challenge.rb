83f077da6ffa8a56b9b060eb4c1b0c9f5b600ca2
Beth Tenorio <betsky98@gmail.com>
2013-09-02 20:50:12 -0400

    Fix methods to calculate insurance

--- abc_challenge.rb
+++ abc_challenge.rb
@@ -11,28 +11,28 @@ string = 'aacacabaaaaacccbccbcccababbccbcccbbbbbbacabbacaaaabbbabcabaccbaccabaaa
 array = [s0, s1, s2, s3, s5, string]
 
 # make an array of letter indexes
-  puts "Sample string: 'abcabc'"
-  a_indexes = [0, 3]
-  b_indexes = [1, 4]
-  c_indexes = [2, 5]
+puts "Sample string: 'abcabc'"
+a_indexes = [0, 3]
+b_indexes = [1, 4]
+c_indexes = [2, 5]
 
 
-  def build_abc(a_indexes, b_indexes, c_indexes)
-    count = 0
-    a_indexes.each do |a|
-      #puts a
-      b_indexes.each do |b|
-        #puts b
-        c_indexes.each do |c|
-          #puts c
-          if a < b && b < c
-            puts "Valid abc found: #{a}, #{b}, #{c}"
-            count += 1
-          end
+def build_abc(a_indexes, b_indexes, c_indexes)
+  count = 0
+  a_indexes.each do |a|
+    b_indexes.each do |b|
+      c_indexes.each do |c|
+        if a < b && b < c
+          puts "Valid abc found: #{a}, #{b}, #{c}"
+          count += 1
         end
       end
     end
-    return count
   end
-  puts build_abc(a_indexes, b_indexes, c_indexes)
+  return count
+end
+puts build_abc(a_indexes, b_indexes, c_indexes)
+
+def build_indexes(string,letter)
+
 
--- mortgage_calc.rb
+++ mortgage_calc.rb
@@ -13,45 +13,51 @@ class HomePurchaseOption
   #the amount of cash value you have in the home
   #This would be difference in your property's value
   #and your mortgage balance
-  def equity_after_sale()
-    @property_value - (@selling_price - @down_payment)
+  def equity_after_sale(property_value, selling_price, down_payment)
+    property_value - (selling_price - down_payment)
   end
 
   #the amount of money you must borrow to purchase the home
-  def required_mortgage()
-    @selling_price - @down_payment
+  def required_mortgage(selling_price, down_payment)
+    selling_price - down_payment
   end
 
   #how much your insurance cost will cost over `years` years
   #Mortgage insurance is 0.5% per year of the total mortgage.
-  def insurance_cost(years)
-    years * 0.05 * (@selling_price - @down_payment)
+  def insurance_cost(selling_price, down_payment, years)
+    years * 0.005 * (selling_price - down_payment)
   end
 
-  private
   #determine if the purchaser must pay insurance
   #If you have less than 20% of your property's value in equity, you typically have to pay mortgage insurance
-  def pmi_required?
-    if @property_value - (@selling_price - @down_payment) < @property_value * 0.2
+  def pmi_required?(property_value, selling_price, down_payment)
+    puts "Property value: #{property_value}"
+    foo = property_value - (selling_price - down_payment)
+    bar = property_value * 0.2
+    puts "foo: #{foo} bar: #{bar}"
+    ratio = foo / property_value
+    puts "ratio: #{ratio}"
+    if property_value - (selling_price - down_payment) < property_value * 0.2
       return true
     else
       return false
+    end
   end
 end
 
 CSV.foreach('property_details.csv', headers: true) do |row|
 
   address = row[0]
-  property_value = row[1]
-  selling_price = row[2]
-  down_payment = row[3]
+  property_value = row[1].to_f
+  selling_price = row[2].to_f
+  down_payment = row[3].to_f
 
-
-  home = Home.new
+  home = HomePurchaseOption.new(address, property_value, selling_price, down_payment)
+  puts ''
   puts "***#{address}***"
-  puts "Mortgage: #{home.required_mortgage}"
-  if home.pmi_required?
-    puts "2 Years of insurance: #{home.required_mortgage}"
+  puts "Mortgage: #{home.required_mortgage(selling_price, down_payment)}"
+  if home.pmi_required?(property_value, selling_price, down_payment)
+    puts "2 Years of insurance: #{home.insurance_cost(selling_price, down_payment, 2)}"
   end
 end
 
