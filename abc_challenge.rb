f233bdcd50c3ec3504199b16e33f15dcf1cc1590
Beth Tenorio <betsky98@gmail.com>
2013-09-04 11:02:24 -0400

    Use findall and detect in building indexes

--- abc_challenge.rb
+++ abc_challenge.rb
@@ -1,51 +1,19 @@
-# s0 = 'abc'  #1
-# s1 = 'aabc' #2
-# s2 = 'aabbc' #4
-# s3 = 'abcbc' #3
-# s4 = 'abcabc' #4
-# s5 = 'bcd' #0
  string = 'aacacabaaaaacccbccbcccababbccbcccbbbbbbacabbacaaaabbbabcabaccbaccabaaabccabacbabaaabaabcabacccabccaababcbacaaabaccaccacccbabbbbbaaacaaacacccaaabcbcabbacccccbaaacbbaccbbcaabcbabbaabbacbccccbaaccccbccacbaaabcaaacaabbcabcbabcbaccbaacbaaabcacccbbabccbbbcacabccccabbcaaaaacaabbaaccabbbccababbbcbabaabcbaabcacbaaaaccccbbcccacabbacccacaaaccbabaabbbcabbabac'
-#pattern = /a+b+c/
-#puts s3.scan(pattern)
+s0 = 'abc'  #1
+s1 = 'aabc' #2
+s2 = 'aabbc' #4
+s3 = 'abcbc' #3
+s4 = 'abcabc' #4
+s5 = 'bcd' #0
 
-
-
-# array = [s0, s1, s2, s3, s5, string]
-
-# # make an array of letter indexes
-# puts "Sample string: 'abcabc'"
-# a_indexes = [0, 3]
-# b_indexes = [1, 4]
-# c_indexes = [2, 5]
-
-
-def build_abc(a_indexes, b_indexes, c_indexes)
-  count = 0
-  a_indexes.each do |a|
-    b_indexes.each do |b|
-      c_indexes.each do |c|
-        if a < b && b < c
-          #puts "Valid abc found: #{a}, #{b}, #{c}"
-          count += 1
-        end
-      end
-    end
-  end
-  return count
-end
-# puts build_abc(a_indexes, b_indexes, c_indexes)
-
-# string = ''
-
-# File.open('abc_challenge.txt', 'r') do |f|
-#   string << f.read
-# end
+strings = [s0, s1, s2, s3, s4, s5]
 
 def build_indexes(string)
   a_indexes = []
   b_indexes = []
   c_indexes = []
   count = 0
+  total = 0
   string.each_char do |letter|
     case
       when letter == 'a'
@@ -59,15 +27,16 @@ def build_indexes(string)
   end
   a_indexes.each do |a|
     b_count = b_indexes.find_all { |b| b > a}
-    first_b = b_indexes.detect { |b| b > a}
+    first_b = (b_indexes.detect { |b| b > a}).to_i
     c_count = c_indexes.find_all { |c| c > first_b}
-    puts b_count.size, c_count.size
+    first_c = (c_indexes.detect { |c| c > first_b}).to_i
+    puts "#{a}, #{first_b}, #{first_c}"
+    total +=  b_count.size * c_count.size
   end
-
-
-
-  #puts c_indexes.inspect
-  #puts build_abc(a_indexes, b_indexes, c_indexes)
+    puts total
 end
-build_indexes(string)
-print string
+
+strings.each do |s|
+  puts s
+  build_indexes(s)
+end
