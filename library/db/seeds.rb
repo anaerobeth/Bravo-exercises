ac85d571dd604048812a665290d159a8b1941909
Beth Tenorio <betsky98@gmail.com>
2013-09-05 15:33:27 -0400

    Add seed data for books

--- library/db/seeds.rb
+++ library/db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+list_of_books =
+[
+  {
+    title: 'Moby Dick',
+    created_at: '2012,2,3',
+    updated_at: '2013,2,3',
+    author: 'Herman Melville',
+    favorite: true
+  },
+  {
+    title: 'Jude the Obscure',
+    created_at: '2002,2,3',
+    updated_at: '2010,2,3',
+    author: 'Thomas Hardy',
+    favorite: true
+  }
+]
+list_of_books.each do |attributes|
+  book = Book.where({
+    title: attributes[:title],
+    created_at: attributes[:created_at],
+    updated_at: attributes[:updated_at],
+    author: attributes[:author],
+    favorite: attributes[:favorite]
+  }).first
+
+  if book.nil?
+    book = Book.new(attributes)
+  else
+    book.assign_attributes = attributes
+  end
+
+  book.save!
+end
